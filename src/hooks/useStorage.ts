cef455a5bbe141d5a273eb0560506868b70a7e7b
Andy <30775786+andrewk604@users.noreply.github.com>
2022-04-24 18:42:28 +0200

    1

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -1,22 +1,23 @@
 /* eslint-disable */
-import React, { useState, useEffect } from "react";
-import { eventDispatch } from "./useEvents";
+// @ts-nocheck
+import React, { useState, useEffect } from "react"
+import { eventDispatch } from "./useEvents"
 
 export let putStorage = (path: any, value: any) => {
   localStorage.setItem(
     path,
     path == `auth_token` ? value : JSON.stringify(value)
-  );
-  eventDispatch(`local-storage`, {});
-};
+  )
+  eventDispatch(`local-storage`, {})
+}
 
 export let getStorage = (path: any) => {
-  let item = localStorage.getItem(path);
-  return item;
-};
+  let item = localStorage.getItem(path)
+  return item
+}
 
 export let getComplicatedStorage = (path: string) => {
-  return JSON.parse(localStorage.getItem(path));
-};
+  return JSON.parse(localStorage.getItem(path))
+}
 
 /* eslint-enable */
